bfe1b83f280e0bcc0f40f2fe6a6ac8998e194070
Mayurijadhav1304 <150362931+Mayurijadhav1304@users.noreply.github.com>
2025-04-04 10:43:33 +0530

    Add files via upload

---
+++ assign5/form.php
@@ -0,0 +1,18 @@
+<?php
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['t1'];
+    $mobile = $_POST['t2'];
+    $roll_no = $_POST['t3'];
+    $email = $_POST['t4'];
+    $address = $_POST['t5'];
+
+    echo "<script>alert('Form submitted successfully!'); window.location.href='index.html';</script>";
+}*/
+$name = $_POST['t1'];
+$mobile = $_POST['t2'];
+$roll_no = $_POST['t3'];
+$email = $_POST['t4'];
+$address = $_POST['t5'];
+echo "Record inserted successfully";
+
+?>
